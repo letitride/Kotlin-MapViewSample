507a6d387161ac2c7c70e20201a8cdc4db1b1781
letitride <ichikawa.fumiya@gmail.com>
2019-07-16 09:50:49 +0900

    マップの表示

---
+++ app/src/main/java/jp/letitride/mapsample/MapsActivity.kt
@@ -0,0 +1,26 @@
+package jp.letitride.mapsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+    }
+}
